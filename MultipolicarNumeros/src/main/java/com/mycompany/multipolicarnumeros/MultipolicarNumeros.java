ec19bff42fa4acf5d1ed91b98ee7c9967f85eecd
juancho el programador <juandavidneusa04@gmail.com>
2024-08-01 10:25:33 -0500

    se adiciono la clase multiplicar numeros-java

---
+++ MultipolicarNumeros/src/main/java/com/mycompany/multipolicarnumeros/MultipolicarNumeros.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.multipolicarnumeros;
+
+/**
+ *
+ * @author Usuario
+ */
+public class MultipolicarNumeros {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
